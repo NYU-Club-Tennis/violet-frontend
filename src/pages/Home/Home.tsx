0425f5385b2a24ed7ea63429fbb0a8511f679f37
Gunn2710 <gsukhum@gmail.com>
2025-06-21 00:17:43 +0700

    Added picture on home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,7 +3,19 @@ import React, { FC } from "react";
 const Home: FC = () => {
   return (
     <div>
-      <h1>Hello world!</h1>
+      <div className="w-screen h-screen flex items-center justify-center 
+                      bg-[url('/src/assets/images/svgs/home-photo.svg')] bg-cover bg-center relative">
+        <div className="absolute inset-0 bg-black bg-opacity-50 z-0"></div>
+      </div>
+      <div className="w-screen h-screen flex items-center justify-center bg-gray-200">
+          <h1>hey2</h1>
+      </div>
+      <div className="w-screen h-screen flex items-center justify-center bg-gray-300">
+          <h1>hey3</h1>
+      </div>
+      <div className="w-screen h-screen flex items-center justify-center bg-gray-400">
+          <h1>hey4</h1>
+      </div>
     </div>
   );
 };
